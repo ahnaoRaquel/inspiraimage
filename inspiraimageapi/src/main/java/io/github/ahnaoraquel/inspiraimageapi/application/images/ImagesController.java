048702e441b6067049041f0120913ec42a614d77
Raquel Niehues <niehues.raquel@gmail.com>
2026-01-20 00:13:12 -0300

    Criando a consulta da image por id e retornando a url da imagem

--- inspiraimageapi/src/main/java/io/github/ahnaoraquel/inspiraimageapi/application/images/ImagesController.java
+++ inspiraimageapi/src/main/java/io/github/ahnaoraquel/inspiraimageapi/application/images/ImagesController.java
@@ -1,16 +1,13 @@
 package io.github.ahnaoraquel.inspiraimageapi.application.images;
 
 import io.github.ahnaoraquel.inspiraimageapi.domain.entity.Image;
-import io.github.ahnaoraquel.inspiraimageapi.domain.enums.ImageExtension;
 import io.github.ahnaoraquel.inspiraimageapi.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -40,6 +37,23 @@ public class ImagesController {
         URI imageUri = buildImageUri(savedImage);
 
         return ResponseEntity.created(imageUri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String id) {
+        var possibleImage = imageService.getById(id);
+        if (possibleImage.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        var image = possibleImage.get();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(image.getExtension().getMediaType());
+        headers.setContentLength(image.getSize());
+        headers.setContentDispositionFormData("inline; filename=\"" + image.getFileName() + "\"", image.getFileName());
+
+        return new ResponseEntity<>(image.getFile(), headers, HttpStatus.OK);
     }
 
     private URI buildImageUri(Image imageId) {
--- inspiraimageapi/src/main/java/io/github/ahnaoraquel/inspiraimageapi/domain/service/ImageService.java
+++ inspiraimageapi/src/main/java/io/github/ahnaoraquel/inspiraimageapi/domain/service/ImageService.java
@@ -2,6 +2,10 @@ package io.github.ahnaoraquel.inspiraimageapi.domain.service;
 
 import io.github.ahnaoraquel.inspiraimageapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
     Image save(Image image);
+
+    Optional<Image> getById(String id);
 }
