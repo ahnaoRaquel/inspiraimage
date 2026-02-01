605b24a0932f08f1041fe3be3ad92988590b2e44
Raquel Niehues <niehues.raquel@gmail.com>
2026-02-01 15:42:22 -0300

    Criando a tela de galeria e a busca pelas imagens

--- inspiraimageui/src/app/galeria/page.tsx
+++ inspiraimageui/src/app/galeria/page.tsx
@@ -1,12 +1,61 @@
+'use client';
+
 import { Template, ImageCard } from "@/components";
+import { useImageService} from "@/resources/image/image.service";
+import {useState} from "react";
+import {Image} from "@/resources/image/image.resource";
 
 export default function GaleriaPage() {
+
+    const useService = useImageService();
+    const [images, setImages] = useState<Image[]>([]);
+    const [query, setQuery] = useState<string>("");
+    const [extension, setExtension] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+
+
+    async function searchImages() {
+        setLoading(true);
+        const result = await useService.buscar(query, extension);
+        setImages(result);
+        setLoading(false);
+    }
+
+    function renderImageCard(image : Image) {
+        return (
+            <ImageCard key={image.url}
+                       nome={image.name}
+                       src={image.url}
+                       tamanho={image.size}
+                       extension={image.extension}
+                       dataUpload={image.uploadDate} />
+        )
+    }
+
+    function renderImageCards() {
+        return images.map(renderImageCard);
+    }
+
     return (
-        <Template >
+        <Template loading={loading}>
+
+            <section className="flex flex-col items-center justify-center my-5">
+                <div className="flex space-x-4">
+                    <input type="text"
+                           onChange={event => setQuery(event.target.value)}
+                           className="border px-3 py-2 rounded-lg text-black"/>
+                    <select onChange={event => setExtension(event.target.value)} className="border px-4 py-2 rounded-lg text-black">
+                        <option value="">Todos</option>
+                        <option value="PNG">PNG</option>
+                        <option value="JPEG">JPEG</option>
+                        <option value="GIF">GIF</option>
+                    </select>
+                    <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-700" onClick={searchImages}>Buscar</button>
+                    <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-700" >Adicionar imagem</button>
+                </div>
+            </section>
             <section className="grid grid-cols-3 gap-4 mb-8">
-                <ImageCard />
-                <ImageCard />
-                <ImageCard />
+                {renderImageCards()}
             </section>
         </Template>
     )
--- inspiraimageui/src/components/ImageCard.tsx
+++ inspiraimageui/src/components/ImageCard.tsx
@@ -1,19 +1,40 @@
+'use client';
+
 interface ImageCardProps {
     nome?: string;
-    tamanho?: string;
+    tamanho?: number;
     dataUpload?: string;
     src?: string;
+    extension?: string;
 }
 
 export const ImageCard: React.FC<ImageCardProps> = (props: ImageCardProps) => {
+
+    function download() {
+        window.open(props.src, "_blank");
+    }
+
     return (
         <div className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 hover:shadow-lg hover:-translate-y-2">
-          <img className="h-56 w-full object-cover rouded-t-md" alt="" src={props.src} />
+          <img className="h-56 w-full object-cover rouded-t-md" alt="" src={props.src} onClick={download} />
             <div className="card-body p-4">
                 <h5 className="text-xl font-semibold mb-2 text-black">{props.nome}</h5>
-                <p className="text-black text-base">{props.tamanho}</p>
+                <p className="text-black text-base">{props.extension}</p>
+                <p className="text-black text-base">{formatBytes(props.tamanho)}</p>
                 <p className="text-black text-base">{props.dataUpload}</p>
             </div>
         </div>
     )
-}   
+}
+
+function formatBytes(bytes: number = 0, decimals = 2) {
+    if (!+bytes) return "0 Bytes"
+
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ["Bytes", "KB", "MB", "GB"]
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
