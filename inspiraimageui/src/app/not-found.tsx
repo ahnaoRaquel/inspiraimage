41551c2cce38ddedb6fd6ab5a3bf5a61b40c690b
Raquel Niehues <niehues.raquel@gmail.com>
2026-02-01 23:53:45 -0300

    Fazendo algumas mudanças

---
+++ inspiraimageui/src/app/not-found.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Template } from "@/components";
+
+export default function NotfoundPage(){
+    return (
+        <Template>
+            <section>
+                <h1 className="text-black text-xl text-center">Página não existe!</h1>
+            </section>
+        </Template>
+    )
+}
